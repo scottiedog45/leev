09a9b52fe73b17887c61ab201bd9e338439084f2
Scottiedog45 <scottiedog45@gmail.com>
2018-02-08 23:03:10 -0500

    test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16';
+
 
 
 it('renders without crashing', () => {
