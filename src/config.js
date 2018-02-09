b96898a717d139b7b4eb68304f9ac269c6d6e1e9
Scottiedog45 <scottiedog45@gmail.com>
2018-02-09 00:22:26 -0500

    test

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
 
-process.env.(REACT_APP_API_BASE_URL + '/api') || 'http://localhost:8000/api';
+process.env.REACT_APP_API_BASE_URL  || 'http://localhost:8000/api';
 
 
 
