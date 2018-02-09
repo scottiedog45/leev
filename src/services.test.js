613b181fd4869b086395ffaf0f9a9e1c83995b1a
Scottiedog45 <scottiedog45@gmail.com>
2018-02-09 00:07:28 -0500

    test

--- src/services.test.js
+++ src/services.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Services from './components/services';
 import {shallow} from 'enzyme'
+import store from './store';
 
 
 
 it('renders without crashing', () => {
-  shallow(<Services />);
+  shallow(<Services store={store}/>);
 });
