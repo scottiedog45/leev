597613883de2db0cf3d21fce28d77941d7fe7af2
Scottiedog45 <scottiedog45@gmail.com>
2018-02-08 23:01:30 -0500

    test

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,6 @@ import App from './components/app';
 import enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({adapter: new Adapter()});
-
 
 it('renders without crashing', () => {
   shallow(<App />);
