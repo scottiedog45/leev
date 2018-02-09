64f4c5720e21199c5cf5ad27e691f1e41e9c072c
Scottiedog45 <scottiedog45@gmail.com>
2018-02-09 01:17:30 -0500

    env

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ export const API_BASE_URL =
 
 
 
-  // 'https://frozen-inlet-15221.herokuapp.com/api'
+  'https://frozen-inlet-15221.herokuapp.com/api'
 
 
 
 //make sure netlify is doing build using the heroku environment variable
 //make sure request is going to proper
-process.env.REACT_APP_API_BASE_URL
+// process.env.REACT_APP_API_BASE_URL
 // || 'http://localhost:8000/api';
 // //
