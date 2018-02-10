e6c399b6cc69bb6df5fb45bf28e96afe12fb3830
Scottiedog45 <scottiedog45@gmail.com>
2018-02-09 19:56:54 -0500

    env

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
 export const API_BASE_URL =
 
-REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
+process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
 
 // 'https://frozen-inlet-15221.herokuapp.com/api';
 
-
 //why didn't export const work?
-//make sure netlify is doing build using the heroku environment variable
-//make sure request is going to proper
-
-//
-// //
