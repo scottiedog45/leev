6fdf0f94f5f41ac627ed2b0804547cb64e606e46
Scottiedog45 <scottiedog45@gmail.com>
2018-02-09 19:30:15 -0500

    env test

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
 
-process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
+$REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
 
 // 'https://frozen-inlet-15221.herokuapp.com/api';
 
